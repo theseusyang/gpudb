1eaa9d95c662e5e2d369685bdf8e1c15f2480a66
HallStattMia <14307130159@fudan.edu.cn>
2018-03-30 01:38:39 +0800

    add bitscan_cpu_64bit

--- cuda/bitscan_cpu.cpp
+++ cuda/bitscan_cpu.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
 using namespace std;
-
+#define utype long long
 int main(int argc, char ** argv){
 	freopen("scan.in","r",stdin);
 	freopen("scan.out","w",stdout);
@@ -10,16 +11,21 @@ int main(int argc, char ** argv){
 	int n = 32;
 	scanf("%d",&n);
 
-	int bytes = n * sizeof(int);
+	utype bytes = n * sizeof(utype);
 
-	int *a;
-	a = (int*)malloc(bytes );   
+	utype *a;
+	a = (utype*)malloc(bytes );   
 
 
 	for (int i = 0; i < n; i++)
-		scanf("%d",a+i);
-	int constC=0;
-	scanf("%d", &constC);
+		if(sizeof(utype)==4)scanf("%d",a+i);
+		else scanf("%lld",a+i);
+	utype constC=0;
+	int test_num = 0;
+	scanf("%d",&test_num);
+	for(int i = 1;i<=test_num;i++)
+	if(sizeof(utype)==4)scanf("%d", &constC);
+	else scanf("%lld",&constC);
 	for(int i=0;i<n;i++)
 		if(a[i]<constC) printf("%d\n",1);
 	else printf("%d\n",0);
